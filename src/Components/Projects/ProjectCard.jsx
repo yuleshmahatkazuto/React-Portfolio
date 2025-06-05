41ec70707cdd9bdd5ea4f6b3fb75a7d13178581c
Yulesh Mahat <yuleshkazuto@gmail.com>
2025-06-05 13:45:01 +1000

    Responsive layour tablet. Fix the overflow and the contact next

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -4,15 +4,15 @@ const ProjectCard = ({ title, toolStack, imageSource }) => {
   return (
     <div className={styles.container}>
       <img src={imageSource} />
-      <div className={styles.overlay}>
-        <p className={styles.button1}>VIEW PROJECT</p>
-        <p className={styles.button2}>VIEW CODE</p>
-      </div>
       <h1 className={styles.title}>{title}</h1>
       <div className={styles.toolStack}>
         {toolStack.map((item, index) => {
           return <p key={index}>{item}</p>;
         })}
+      </div>
+      <div className={styles.overlay}>
+        <p className={styles.button1}>VIEW PROJECT</p>
+        <p className={styles.button2}>VIEW CODE</p>
       </div>
     </div>
   );
